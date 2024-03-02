bb8c7d8a03e4a95d459b61e37ce7994151e44338
Zellione <nagels.sebastian@gmail.com>
2024-03-02 10:55:08 +0100

    day 3 part 2

--- aoc2023/src/day3/day3.rs
+++ aoc2023/src/day3/day3.rs
@@ -2,6 +2,12 @@ use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 
+struct CogAdjacent {
+    cog_row: usize,
+    cog_col: usize,
+    number: u32,
+}
+
 fn print_array(pattern: &[char; num_rows() * num_columns()]) {
     for i in 0..num_rows() {
         for j in 0..num_columns() {
@@ -72,8 +78,62 @@ fn is_num_valid(
     false
 }
 
+fn clamp_row(row: usize) -> (usize, usize) {
+    let mut start_row: usize = row;
+    let mut end_row: usize = row;
+
+    if start_row > 0 {
+        start_row -= 1;
+    }
+
+    if end_row < num_rows() - 1 {
+        end_row += 2;
+    }
+
+    (start_row, end_row)
+}
+
+fn clamp_col(col: usize) -> (usize, usize) {
+    let mut start_col: usize = col;
+    let mut end_col: usize = col;
+
+    if start_col > 0 {
+        start_col -= 1;
+    }
+
+    if end_col < num_columns() - 1 {
+        end_col += 2;
+    }
+
+    (start_col, end_col)
+}
+
+fn is_cog_adjacent(
+    pattern: &[char; num_rows() * num_columns()],
+    row: usize,
+    col_start: usize,
+    col_end: usize,
+) -> (usize, usize, bool) {
+    let (start_row, end_row) = clamp_row(row);
+    let (start_col, _) = clamp_col(col_start);
+    let (_, end_col) = clamp_col(col_end);
+
+    for i_row in start_row..end_row {
+        for i_col in start_col..end_col {
+            let pos = i_row * num_rows() + i_col;
+            if pattern[pos] == '*' {
+                return (i_row, i_col, true);
+            }
+        }
+    }
+
+    (0, 0, false)
+}
+
 fn iterate_nums(pattern: &[char; num_rows() * num_columns()]) {
     let mut sum: u32 = 0;
+    let mut gear_sum: u32 = 0;
+    let mut gears: Vec<CogAdjacent> = Vec::new();
 
     for i_row in 0..num_rows() {
         let mut number: String = String::new();
@@ -90,7 +150,27 @@ fn iterate_nums(pattern: &[char; num_rows() * num_columns()]) {
                 if is_num_valid(pattern, i_row, i_col - number.len(), number.len()) {
                     sum += num;
 
-                    // println!("Found valid number: {}", num);
+                    let (row_c, col_c, is_cog_adjacent) =
+                        is_cog_adjacent(pattern, i_row, i_col - number.len(), i_col - 1);
+                    if is_cog_adjacent {
+                        let mut made_gear_multi = false;
+                        for gear in &gears {
+                            if gear.cog_col == col_c && gear.cog_row == row_c {
+                                gear_sum += gear.number * num;
+                                made_gear_multi = true;
+                                break;
+                            }
+                        }
+
+                        if !made_gear_multi {
+                            let cog_adjacent = CogAdjacent {
+                                cog_row: row_c,
+                                cog_col: col_c,
+                                number: num,
+                            };
+                            gears.push(cog_adjacent);
+                        }
+                    }
                 }
 
                 number = String::new();
@@ -100,22 +180,23 @@ fn iterate_nums(pattern: &[char; num_rows() * num_columns()]) {
 
     println!("-------------------------------");
     println!("The sum of valid numbers is: {}", sum);
+    println!("The sum of valid gear products is: {}", gear_sum);
 }
 
 // WARN: Input has 141 rows
 const fn num_rows() -> usize {
-    const ROWS: usize = 10;
+    const ROWS: usize = 141;
 
     ROWS
 }
 
 // WARN: Input has 141 cols
 const fn num_columns() -> usize {
-    const COLUMNS: usize = 10;
+    const COLUMNS: usize = 141;
 
     COLUMNS
 }
 
 fn main() {
-    let _ = read_file_line_by_line("src/day3/day3_calib");
+    let _ = read_file_line_by_line("src/day3/day3_input");
 }
