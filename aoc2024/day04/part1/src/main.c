24e7df67966de3d19aee230c87c8b603b819f212
Zellione <nagels.sebastian@gmail.com>
2024-12-12 22:21:55 +0100

    aoc2024 day 4 part 1 finish

--- aoc2024/day04/part1/src/main.c
+++ aoc2024/day04/part1/src/main.c
@@ -4,30 +4,72 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Try rewriting like this:
-// Search X and try to expand in all directions and check if chars are alligning and
-// indices are still inbounds
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_RESET "\x1b[0m"
 
 // #define TEST
-#define DEBUG
 
 #define WORD_LENGTH 4
 #ifndef TEST
 #define ARRAY_ROWS 140
 #define ARRAY_COLS 140
-#define LINE_LENGTH 140
 #define INPUT_FILE "../../../day4_input"
 #else
 #define ARRAY_ROWS 10
 #define ARRAY_COLS 10
-#define LINE_LENGTH 10
-#define INPUT_FILE "../../input_test2"
+#define INPUT_FILE "../../input_test"
 #endif
 
-#ifdef DEBUG
-char tmp_test[ARRAY_COLS * ARRAY_ROWS] = {0};
-int tmp_indices[WORD_LENGTH]           = {0};
-#endif
+int indices_found[ARRAY_ROWS * ARRAY_COLS] = {0};
+int indices_index                          = 0;
+int record_indices[WORD_LENGTH]            = {0};
+
+void record_index(int index, int pos)
+{
+    record_indices[pos] = index;
+}
+
+void reset_indices()
+{
+    for (int i = 0; i < WORD_LENGTH; i++)
+    {
+        record_indices[i] = 0;
+    }
+}
+
+void add_indices()
+{
+    for (int i = 0; i < WORD_LENGTH; i++)
+    {
+        indices_found[indices_index] = record_indices[i];
+        indices_index++;
+    }
+
+    reset_indices();
+}
+
+bool is_found_index(int index)
+{
+    for (int i = 0; i <= indices_index; i++)
+    {
+        if (indices_found[i] == index)
+            return true;
+    }
+
+    return false;
+}
+
+enum Directions
+{
+    LEFT,
+    TOPLEFT,
+    TOP,
+    TOPRIGHT,
+    RIGHT,
+    BOTTOMRIGHT,
+    BOTTOM,
+    BOTTOMLEFT
+};
 
 void clean_buffer(char* buffer, int buffer_length)
 {
@@ -39,11 +81,10 @@ void clean_buffer(char* buffer, int buffer_length)
 
 void read_data(char* line, char* data, int line_number)
 {
-    // printf("Buffer POS: %i %i %i\n", line_number, LINE_LENGTH, LINE_LENGTH * line_number);
-    for (int i = 0; i < LINE_LENGTH; i++)
+    for (int i = 0; i < ARRAY_COLS; i++)
     {
-        assert(line_number * LINE_LENGTH + i < ARRAY_COLS * ARRAY_ROWS);
-        data[line_number * LINE_LENGTH + i] = line[i];
+        assert(line_number * ARRAY_COLS + i < ARRAY_COLS * ARRAY_ROWS);
+        data[line_number * ARRAY_COLS + i] = line[i];
     }
 }
 
@@ -53,160 +94,208 @@ void print_data(char* data)
     {
         for (int j = 0; j < ARRAY_COLS; j++)
         {
-            // printf("i: %i; j: %i index: %i\n", i, j, i * LINE_LENGTH + j);
-            printf("%c", data[i * LINE_LENGTH + j]);
+            int temp = i * ARRAY_COLS + j;
+            if (is_found_index(temp))
+            {
+                printf(ANSI_COLOR_RED "%c" ANSI_COLOR_RESET, data[temp]);
+            }
+            else
+            {
+                printf("%c", data[temp]);
+            }
         }
         printf("\n");
     }
 }
 
-int check_horizontal(char* data)
+bool is_xmas(char* buffer)
 {
-    char tmp_buffer[WORD_LENGTH + 1] = {0};
-    int words_found                  = 0;
-    for (int i = 0; i < ARRAY_ROWS; i++)
+    if (strstr(buffer, "XMAS") != NULL)
     {
-        for (int j = 0; j < ARRAY_COLS - WORD_LENGTH; j++)
-        {
-            for (int k = 0; k < WORD_LENGTH; k++)
-            {
-#ifdef DEBUG
-                tmp_indices[k] = i * LINE_LENGTH + j + k;
-#endif
-                tmp_buffer[k] = data[i * LINE_LENGTH + j + k];
-            }
-
-            tmp_buffer[WORD_LENGTH] = '\0';
-
-            if (strstr(tmp_buffer, "XMAS") != NULL || strstr(tmp_buffer, "SAMX") != NULL)
-            {
-#ifdef DEBUG
-                for (int k = 0; k < WORD_LENGTH; k++)
-                    tmp_test[tmp_indices[k]] = tmp_buffer[k];
-#endif
-                words_found++;
-            }
-
-            clean_buffer(tmp_buffer, WORD_LENGTH + 1);
-#ifdef DEBUG
-            for (int k = 0; k < WORD_LENGTH; k++)
-                tmp_indices[k] = 0;
-#endif
-        }
+        add_indices();
+        return true;
     }
-
-    printf("Words found horizonal: %i\n", words_found);
-    return words_found;
+    reset_indices();
+    return false;
 }
 
-int check_vertical(char* data)
+int expand_right(char* data, int row, int col)
 {
-    char tmp_buffer[WORD_LENGTH + 1] = {0};
-    int words_found                  = 0;
+    if (!(col + WORD_LENGTH - 1 < ARRAY_COLS))
+        return 0;
 
-    for (int i = 0; i <= ARRAY_COLS; i++)
+    char word[5] = {0};
+    int index    = 0;
+
+    for (int i = col; i < col + WORD_LENGTH; i++)
     {
-        for (int j = 0; j <= ARRAY_ROWS - WORD_LENGTH; j++)
-        {
-            for (int k = 0; k < WORD_LENGTH; k++)
-            {
-
-#ifdef DEBUG
-                tmp_indices[k] = (j + k) * LINE_LENGTH + i;
-#endif
-                tmp_buffer[k] = data[(j + k) * LINE_LENGTH + i];
-            }
-
-            tmp_buffer[WORD_LENGTH] = '\0';
-
-            if (strstr(tmp_buffer, "XMAS") != NULL || strstr(tmp_buffer, "SAMX") != NULL)
-            {
-#ifdef DEBUG
-                for (int k = 0; k < WORD_LENGTH; k++)
-                    tmp_test[tmp_indices[k]] = tmp_buffer[k];
-#endif
-                words_found++;
-            }
-
-            clean_buffer(tmp_buffer, WORD_LENGTH + 1);
-#ifdef DEBUG
-            for (int k = 0; k < WORD_LENGTH; k++)
-                tmp_indices[k] = 0;
-#endif
-        }
+        record_index(row * ARRAY_COLS + i, index);
+        word[index] = data[row * ARRAY_COLS + i];
+        index++;
     }
+    word[WORD_LENGTH] = '\0';
 
-    printf("Words found vertical: %i\n", words_found);
-    return words_found;
+    return is_xmas(word) ? 1 : 0;
 }
 
-int check_diagonal(char* data)
+int expand_left(char* data, int row, int col)
 {
-    char tmp_buffer[WORD_LENGTH + 1] = {0};
-    int words_found                  = 0;
-    int current_index                = 0;
+    if (!(col - (WORD_LENGTH - 1) >= 0))
+        return 0;
 
-    for (int i = 0; i <= ARRAY_ROWS - WORD_LENGTH; i++)
+    char word[5] = {0};
+    int index    = 0;
+
+    for (int i = col; i > col - WORD_LENGTH - 1; i--)
     {
-        for (int j = 0; j <= ARRAY_COLS - WORD_LENGTH; j++)
-        {
-            for (int k = 0; k < WORD_LENGTH; k++)
-            {
-#ifdef DEBUG
-                tmp_indices[k] = (i + k) * LINE_LENGTH + (j + k);
-#endif
-                tmp_buffer[k] = data[(i + k) * LINE_LENGTH + (j + k)];
-            }
-            tmp_buffer[WORD_LENGTH] = '\0';
+        record_index(row * ARRAY_COLS + i, index);
+        word[index] = data[row * ARRAY_COLS + i];
+        index++;
+    }
+    word[WORD_LENGTH] = '\0';
 
-            if (strstr(tmp_buffer, "XMAS") != NULL || strstr(tmp_buffer, "SAMX") != NULL)
-            {
-#ifdef DEBUG
-                for (int k = 0; k < WORD_LENGTH; k++)
-                    tmp_test[tmp_indices[k]] = tmp_buffer[k];
-#endif
-                words_found++;
-            }
+    return is_xmas(word) ? 1 : 0;
+}
 
-            clean_buffer(tmp_buffer, WORD_LENGTH + 1);
-#ifdef DEBUG
-            for (int k = 0; k < WORD_LENGTH; k++)
-                tmp_indices[k] = 0;
-#endif
-        }
+int expand_top(char* data, int row, int col)
+{
+    if (!(row - (WORD_LENGTH - 1) >= 0))
+        return 0;
+
+    char word[5] = {0};
+    int index    = 0;
+
+    for (int i = row; i > row - WORD_LENGTH; i--)
+    {
+        record_index(i * ARRAY_COLS + col, index);
+        word[index] = data[i * ARRAY_COLS + col];
+        index++;
+    }
+    word[WORD_LENGTH] = '\0';
+
+    return is_xmas(word) ? 1 : 0;
+}
+
+int expand_bottom(char* data, int row, int col)
+{
+    if (!(row + WORD_LENGTH - 1 < ARRAY_ROWS))
+        return 0;
+
+    char word[5] = {0};
+    int index    = 0;
+
+    for (int i = row; i < row + WORD_LENGTH; i++)
+    {
+        record_index(i * ARRAY_COLS + col, index);
+        word[index] = data[i * ARRAY_COLS + col];
+        index++;
+    }
+    word[WORD_LENGTH] = '\0';
+
+    return is_xmas(word) ? 1 : 0;
+}
+
+int expand_top_left(char* data, int row, int col)
+{
+    // imagine row 9 col 3
+    if (!(row - (WORD_LENGTH - 1) >= 0 && col - (WORD_LENGTH - 1) >= 0))
+        return 0;
+
+    char word[5] = {0};
+
+    for (int i = 0; i < WORD_LENGTH; i++)
+    {
+        record_index((row - i) * ARRAY_COLS + (col - i), i);
+        word[i] = data[(row - i) * ARRAY_COLS + (col - i)];
+    }
+    word[WORD_LENGTH] = '\0';
+
+    return is_xmas(word) ? 1 : 0;
+}
+
+int expand_top_right(char* data, int row, int col)
+{
+    if (!(row - (WORD_LENGTH - 1) >= 0 && col + WORD_LENGTH - 1 < ARRAY_COLS))
+        return 0;
+
+    char word[5] = {0};
+
+    for (int i = 0; i < WORD_LENGTH; i++)
+    {
+        record_index((row - i) * ARRAY_COLS + (col + i), i);
+        word[i] = data[(row - i) * ARRAY_COLS + (col + i)];
+    }
+    word[WORD_LENGTH] = '\0';
+
+    return is_xmas(word) ? 1 : 0;
+}
+
+int expand_bottom_right(char* data, int row, int col)
+{
+    if (!(row + WORD_LENGTH - 1 < ARRAY_ROWS && col + WORD_LENGTH - 1 < ARRAY_COLS))
+        return 0;
+
+    char word[5] = {0};
+
+    for (int i = 0; i < WORD_LENGTH; i++)
+    {
+        record_index((row + i) * ARRAY_COLS + (col + i), i);
+        word[i] = data[(row + i) * ARRAY_COLS + (col + i)];
+    }
+    word[WORD_LENGTH] = '\0';
+
+    return is_xmas(word) ? 1 : 0;
+}
+
+int expand_bottom_left(char* data, int row, int col)
+{
+    if (!(row + WORD_LENGTH - 1 < ARRAY_ROWS && col - (WORD_LENGTH - 1) >= 0))
+        return 0;
+
+    char word[5] = {0};
+
+    for (int i = 0; i < WORD_LENGTH; i++)
+    {
+        record_index((row + i) * ARRAY_COLS + (col - i), i);
+        word[i] = data[(row + i) * ARRAY_COLS + (col - i)];
+    }
+    word[WORD_LENGTH] = '\0';
+
+    return is_xmas(word) ? 1 : 0;
+}
+
+int expand(char* data, int row, int col, enum Directions dir)
+{
+    int words_found = 0;
+
+    switch (dir)
+    {
+    case LEFT:
+        words_found = expand_left(data, row, col);
+        break;
+    case TOPLEFT:
+        words_found = expand_top_left(data, row, col);
+        break;
+    case TOP:
+        words_found = expand_top(data, row, col);
+        break;
+    case TOPRIGHT:
+        words_found = expand_top_right(data, row, col);
+        break;
+    case RIGHT:
+        words_found = expand_right(data, row, col);
+        break;
+    case BOTTOMRIGHT:
+        words_found = expand_bottom_right(data, row, col);
+        break;
+    case BOTTOM:
+        words_found = expand_bottom(data, row, col);
+        break;
+    case BOTTOMLEFT:
+        words_found = expand_bottom_left(data, row, col);
+        break;
     }
 
-    for (int i = 0; i <= ARRAY_ROWS - WORD_LENGTH; i++)
-    {
-        for (int j = ARRAY_COLS; j >= WORD_LENGTH; j--)
-        {
-            for (int k = 0; k < WORD_LENGTH; k++)
-            {
-#ifdef DEBUG
-                tmp_indices[k] = ((i + k) * LINE_LENGTH) - 1 + (j - k);
-#endif
-                tmp_buffer[k] = data[((i + k) * LINE_LENGTH) - 1 + (j - k)];
-            }
-            tmp_buffer[WORD_LENGTH] = '\0';
-
-            if (strstr(tmp_buffer, "XMAS") != NULL || strstr(tmp_buffer, "SAMX") != NULL)
-            {
-#ifdef DEBUG
-                for (int k = 0; k < WORD_LENGTH; k++)
-                    tmp_test[tmp_indices[k]] = tmp_buffer[k];
-#endif
-                words_found++;
-            }
-
-#ifdef DEBUG
-            for (int k = 0; k < WORD_LENGTH; k++)
-                tmp_indices[k] = 0;
-#endif
-            clean_buffer(tmp_buffer, WORD_LENGTH + 1);
-        }
-    }
-
-    printf("Words found diagonal: %i\n", words_found);
     return words_found;
 }
 
@@ -214,8 +303,20 @@ int check(char* data)
 {
     int words_found = 0;
 
-    words_found += check_horizontal(data);
-    // words_found += check_vertical(data); words_found += check_diagonal(data);
+    for (int i = 0; i < ARRAY_ROWS; i++)
+    {
+        for (int j = 0; j < ARRAY_COLS; j++)
+        {
+            if (data[i * ARRAY_COLS + j] != 'X')
+                continue;
+
+            int temp = 0;
+            for (int k = LEFT; k <= BOTTOMLEFT; k++)
+            {
+                words_found += expand(data, i, j, k);
+            }
+        }
+    }
 
     return words_found;
 }
@@ -225,7 +326,7 @@ int main(int argc, char* argv[])
     uint accumulated = 0;
 
     FILE* fp;
-    char line[LINE_LENGTH + 1] = {0};
+    char line[ARRAY_COLS + 1] = {0};
     ssize_t read;
 
     char data[ARRAY_COLS * ARRAY_ROWS];
@@ -236,7 +337,7 @@ int main(int argc, char* argv[])
         exit(EXIT_FAILURE);
 
     int buffer_pos = 0;
-    while (fgets(line, LINE_LENGTH + 1, fp))
+    while (fgets(line, ARRAY_COLS + 1, fp))
     {
         if (line[0] == '\n')
             continue;
@@ -245,19 +346,9 @@ int main(int argc, char* argv[])
     }
     fclose(fp);
 
-#ifdef DEBUG
-    for (int i = 0; i < ARRAY_ROWS; i++)
-        for (int j = 0; j < ARRAY_COLS; j++)
-            tmp_test[i * LINE_LENGTH + j] = '.';
-#endif
-
     accumulated += check(data);
-    print_data(data);
-#ifdef DEBUG
-    printf("\n");
-    print_data(tmp_test);
-#endif
 
+    print_data(data);
     printf("\nThis is what I calculated: %i\n", accumulated);
 
     return 0;
